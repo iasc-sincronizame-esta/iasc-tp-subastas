e98050b2c12812f97fe1dc3848ee8c194f31fa44
Aldana Quintana <aldanaqm@gmail.com>
2015-11-19 16:35:47 -0300

    Fix some errors

--- lib/modules/subastero.ex
+++ lib/modules/subastero.ex
@@ -4,33 +4,32 @@ defmodule Subastero do
   def start() do
     subastas = %{}
     compradores = %{}
-    usuarios = %{}
-    spawn fn -> loop({ subastas, compradores, usuarios }) end
+    spawn fn -> loop({ subastas, compradores }) end
   end
 
-  def notificar(interesados, mensaje) do
-    Enum.each(interesados, fn(interesado) -> send elem(interesado, 0), mensaje end)
+  def notificar(interesados, mensaje, get_pid \\ fn(interesado) -> interesado[:pid] end) do
+    Enum.each(interesados, fn(interesado) -> send get_pid.(interesado), mensaje end)
   end
 
-  def loop({subastas, compradores, usuarios}) do
+  def loop({subastas, compradores}) do
     receive do
       { :crear_usuario, pid_usuario, nombre } ->
 
-        id_usuario = make_ref
+        id_usuario =  :random.uniform(1000000)
 
-        datos_usuario =
+        datos_comprador =
           %{
-            pid_vendedor: pid_usuario,
+            pid: pid_usuario,
             nombre: nombre
           }
 
-        usuarios = Map.put(usuarios, id_usuario, datos_usuario)
+        compradores = Map.put(compradores, id_usuario, datos_comprador)
 
         IO.puts "ATENCIÓN! TENEMOS UN NUEVO USUARIO: #{nombre}"
 
       { :crear_subasta, pid_vendedor, titulo, precio_base, duracion } ->
 
-        id_subasta = make_ref
+        id_subasta =  :random.uniform(1000000)
         datos_subasta =
           %{
             pid_vendedor: pid_vendedor,
@@ -38,10 +37,10 @@ defmodule Subastero do
             precio_base: precio_base,
             duracion: duracion,
             compradores: HashSet.new
-        }
+          }
         subastas = Map.put(subastas, id_subasta, datos_subasta)
 
-        notificar(compradores, { :nueva_subasta, datos_subasta} )
+        notificar(Map.values(compradores), { :nueva_subasta, datos_subasta} )
 
         IO.puts "ATENCIÓN! TENEMOS UNA NUEVA SUBASTA: #{titulo}"
 
@@ -64,23 +63,23 @@ defmodule Subastero do
            }
           )
 
-          notificar([{pid_comprador}], { :ok, "Tu oferta esta primero en #{subasta[:titulo]}!"})
+          notificar([%{pid: pid_comprador}], { :ok, "Tu oferta esta primero en #{subasta[:titulo]}!"})
 
           diferencia = HashSet.new
           diferencia = Set.put(diferencia, pid_comprador)
 
-          notificar(Set.difference(subasta[:compradores], diferencia),
+          notificar(Map.values(Set.difference(subasta[:compradores], diferencia)),
             { :nueva_oferta, "Hubo una nueva oferta en: #{subasta[:titulo]} de $ #{oferta}"})
 
           IO.puts "ATENCION: La nueva oferta fue realizada con exito"
 
         else
-          notificar([{pid_comprador}], {:ok, "Tu oferta fue insuficiente"})
+          notificar([%{pid: pid_comprador}], {:ok, "Tu oferta fue insuficiente"})
         end
       { :listar_subastas, pid_usuario } ->
-        notificar([{ pid_usuario }], {:ok, subastas})
+        notificar([%{pid:  pid_usuario }], {:ok, subastas})
 
-      loop({ subastas, compradores, usuarios })
     end
+    loop({ subastas, compradores })
   end
 end
