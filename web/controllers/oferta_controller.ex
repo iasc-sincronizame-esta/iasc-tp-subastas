7d64e2e127607abfacc37ff9173fc7f89fa5d5c5
Aldana Quintana <aldanaqm@gmail.com>
2015-11-27 03:30:22 -0300

    Borro lineas comentadas

--- web/controllers/oferta_controller.ex
+++ web/controllers/oferta_controller.ex
@@ -6,10 +6,6 @@ defmodule IascTpSubastas.OfertaController do
   plug :scrub_params, "oferta" when action in [:create]
 
   def create(conn, %{"id_subasta" => id_subasta, "id_comprador" => id_comprador, "oferta" => oferta}) do
-    # {id_subasta, _} = Integer.parse(id_subasta)
-    # {id_comprador, _} = Integer.parse(id_comprador)
-    # {oferta, _} = Integer.parse(oferta)
-
     result = Subastero.ofertar(id_subasta, id_comprador, oferta)
 
     if oferta != nil do
